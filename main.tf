8785840d8404353de5afbd385d6d227248ef4f79
marciogoda <marcio.goda@iongroup.com>
2026-02-06 12:53:04 +0000

    IONAOPS-77 fix addon versions

--- main.tf
+++ main.tf
@@ -63,49 +63,54 @@ resource "aws_eks_node_group" "node_group" {
 }
 
 data "aws_eks_addon_version" "vpc_cni"{
-    addon_name = "vpc-cni"
+    addon_name         = "vpc-cni"
     kubernetes_version = aws_eks_cluster.cluster.version
+    most_recent        = true
 }
 
 resource "aws_eks_addon" "vpc_cni" {
     cluster_name    = aws_eks_cluster.cluster.name
     addon_name      = "vpc-cni"
-    addon_version   = data.aws_eks_addon_version.vpc_cni.version.latest_version
+    addon_version   = data.aws_eks_addon_version.vpc_cni.version.version
     depends_on      = [aws_eks_cluster.cluster]
 }
 
 data "aws_eks_addon_version" "kube_proxy"{
     addon_name = "kube-proxy"
     kubernetes_version = aws_eks_cluster.cluster.version
+    most_recent        = true
 }
 resource "aws_eks_addon" "kube_proxy" {
     cluster_name    = aws_eks_cluster.cluster.name
     addon_name      = "kube-proxy"
-    addon_version   = data.aws_eks_addon_version.kube_proxy.version.latest_version
+    addon_version   = data.aws_eks_addon_version.kube_proxy.version.version
     depends_on      = [aws_eks_cluster.cluster]
 }
 
 data "aws_eks_addon_version" "core_dns"{
-    addon_name = "coredns"
+    addon_name         = "coredns"
     kubernetes_version = aws_eks_cluster.cluster.version
+    most_recent        = true
 }
 
 resource "aws_eks_addon" "core_dns" {
     cluster_name    = aws_eks_cluster.cluster.name
     addon_name      = "coredns"
-    addon_version   = data.aws_eks_addon_version.core_dns.version.latest_version
+    addon_version   = data.aws_eks_addon_version.core_dns.version.version
     depends_on      = [aws_eks_cluster.cluster]
 }
 
 data "aws_eks_addon_version" "pod_identity"{
-    addon_name = "eks-pod-identity-agent"
+    addon_name         = "eks-pod-identity-agent"
     kubernetes_version = aws_eks_cluster.cluster.version
+    most_recent        = true
+
 }
 
 resource "aws_eks_addon" "pod_identity" {
     cluster_name    = aws_eks_cluster.cluster.name
     addon_name      = "eks-pod-identity-agent"
-    addon_version   = data.aws_eks_addon_version.pod_identity.version.latest_version
+    addon_version   = data.aws_eks_addon_version.pod_identity.version.version
     depends_on      = [aws_eks_cluster.cluster]
 }
 
